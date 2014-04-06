0ad63b8a51e31008f96c0ed605297404dc0e8031
michal.papciak <michal.papciak@cognifide.com>
2014-04-06 17:29:06 +0200

    Refactored data processing, added code to the method recordUserActivities

--- custom_modules/dataProcessing.js
+++ custom_modules/dataProcessing.js
@@ -4,11 +4,13 @@ var socket = require('socket.io');
 var io = socket.listen(app);
 
 
-var timestamp,
+var //global settings
+	visitDuration,
 	// storage variables
 	visitorsActiveOnWebsite = new Array();
 	trackingObject = {};
 
+//utils
 var addActiveVisitor = function(visitorID) {
 	visitorsActiveOnWebsite.push(visitorID);
 };
@@ -18,18 +20,23 @@ var removeInactiveVisitor = function(visitorID) {
 	visitorsActiveOnWebsite.splice(index, 1);
 };
 
-var getNumOfActiveVisitors = function() {
-	return visitorsActiveOnWebsite.length;
-};
-
-var getActiveVisitors = function() {
-	return visitorsActiveOnWebsite;
-};
-
-//util
 // function to calculate timestamp differences 
+function calculateTimestampDifference(date1, date2, maxAllowedMinutesDifference){
+	var oneMinute = 1000 * 60;
+	var date1Miliseconds = date1.getTime();
+	var date2Miliseconds = date2.getTime();
+	var milisecondsDifference = date2Miliseconds - date1Miliseconds;
+	var minutesDifference = Math.round(milisecondsDifference / oneMinute);
+	if (minutesDifference > maxAllowedMinutesDifference) {
+		return true;
+	}
+	else {
+		return false;
+	}
+};
 
-var recordVisit = function(eventToListen, visitDuration, accountName) {
+//data processing methods
+var recordUserActivities = function(accountName) {
 	/* eventToListen - clientSide emits pageData (and in future others) events. This in theory could be changed to any other event name. This method listens to a parameter
 	 * passed here. In future this might be an options object, which will pass an array of multiple events - trackLinks, customEvents, etc.
 	 * 
@@ -43,8 +50,47 @@ var recordVisit = function(eventToListen, visitDuration, accountName) {
 	//   visJourney = Array of PageData objects (in future other tracking)
 	//   currentPage = val,
 	//   ?? visitDuration }
-	
+	io.sockets.on('connection', function(client) {
+		client.on('sendPageData', function(pageData) {
+			
+		});
+		
+		
+		client.on('sendLinkData', function(linkData) {
+			
+		});
+		/*
+		 * client.on('sendLinkData', function(linkData));
+		 * 
+		 * client.on('sendCustomData', function(customData));
+		 */
+	});	
 };
 
+//getters
+var getNumOfActiveVisitors = function() {
+	return visitorsActiveOnWebsite.length;
+};
+
+var getActiveVisitors = function() {
+	return visitorsActiveOnWebsite;
+};
+
+var getTrackingObject = function() {
+	return trackingObject;
+};
+
+//setters
+function setServerPortForApp(server) {
+	app.listen(server);
+}
+
+function setVisitDuration(timeInMinutes) {
+	visitDuration = timeInMinutes;
+}
 
 exports.getNumOfActiveVisitors = getNumOfActiveVisitors;
+exports.getTrackingObject = getTrackingObject;
+//settings
+exports.appListenOn = setServerPortForApp;
+exports.setVisitDuration = setVisitDuration;
